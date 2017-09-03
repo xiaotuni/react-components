6dff6cd3378ea7cf4e8d5e226fbf00f58f053ae0
xiaotuni <xiaotuni@gmail.com>
2017-09-03 15:43:57 +0800

    此分支是为了使用React最新版本，但现在有起不来了，正找原因中...

---
+++ src/containers/index.jsx
@@ -0,0 +1,6 @@
+export App from './App/App';
+export Default from './Default/Default';
+
+// export NotFound from './NotFound/NotFound';
+// export Page1 from './Page1/Page1';
+// export Page2 from './Page1/Page2';
